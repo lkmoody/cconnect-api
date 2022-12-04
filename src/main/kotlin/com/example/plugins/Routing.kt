55a696718acacd6c20490be0694811e1692008f2
Larry Moody <lkmoody@gmail.com>
2022-12-03 20:56:47 -0700

    More comments

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -14,6 +14,7 @@ fun Application.configureRouting() {
 
     routing {
         route("/api/v1") {
+            // Health check end point
             get("/health") {
                 call.respond(HttpStatusCode.OK)
             }
