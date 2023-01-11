08fc1e8af66139a8a8af88843a3ec9b4294ed98c
Larry Moody <lkmoody@gmail.com>
2023-01-10 18:43:21 -0700

    Turned down paging to 2

--- src/main/kotlin/com/constituentconnect/routes/VoteRoutes.kt
+++ src/main/kotlin/com/constituentconnect/routes/VoteRoutes.kt
@@ -81,7 +81,7 @@ private fun Route.getVotes() {
             val userIdFilter = call.parameters["userId"]?.toInt() ?: 0
             val pageNumberFilter = call.request.queryParameters["page"]?.toInt() ?: 1
             val statusFilter = call.request.queryParameters["status"]
-            val pageCount = 50
+            val pageCount = 2
             val skip = ((pageNumberFilter - 1) * pageCount).toLong()
 
             // Get the count of all rows to determine pages
